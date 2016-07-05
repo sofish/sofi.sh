248015d87575ba9f085117171919a8abefa246d0
sofish <happinesz@gmail.com>
2016-07-05 11:47:31 +0800

    init for a new program

--- theme/default/js/helpers.js
+++
@@ -1,8 +0,0 @@
-// selector
-const $ = (selector, scope) => {
-  scope = scope || document;
-  if(/^#\S+/.test(selector)) return scope.querySelector(selector);
-  return [].slice.call(scope.querySelectorAll(selector));
-};
-
-export { $ }
